9798ff35ec98944050f0ada3b653f0fe43c0cdf7
maulana4de <maulana4de@gmail.com>
2019-10-19 14:44:23 +0700

    final commit

--- src/config/redux/action/index.js
+++ src/config/redux/action/index.js
@@ -73,23 +73,23 @@ export const addDataToAPI = data => dispatch => {
 
 // getData from firebase
 export const getDataFromAPI = userId => dispatch => {
-  const urlNotes = database
-    .ref("notes/" + userId)
-    .orderByChild("date")
-    .limitToLast(2);
+  const urlNotes = database.ref("notes/" + userId);
+
   return new Promise((resolve, reject) => {
     urlNotes.on("value", function(snapshot) {
       //
-      console.log("get data: ", snapshot.val());
+      // console.log("get data: ", snapshot.val());
       // convert object to array dengan Object.keys
       // manipulasi object ke dalam array
       const data = [];
+
       Object.keys(snapshot.val()).map(key => {
         data.push({
           id: key,
           data: snapshot.val()[key]
         });
       });
+
       dispatch({ type: "SET_NOTES", value: data });
 
       resolve(snapshot.val());
@@ -97,4 +97,33 @@ export const getDataFromAPI = userId => dispatch => {
   });
 };
 
-// perbedaan .set (untuk memperbaharui tidak menambah data ) & .push (untuk menambah data baru)
+// updateData from firebase
+export const updateDataFromAPI = data => dispatch => {
+  const urlNotes = database.ref(`notes/${data.userId}/${data.noteId}`);
+
+  return new Promise((resolve, reject) => {
+    urlNotes.set(
+      {
+        title: data.title,
+        content: data.content,
+        date: data.date
+      },
+      err => {
+        if (err) {
+          reject(false);
+        } else {
+          resolve(true);
+        }
+      }
+    );
+  });
+};
+
+// deleteData from firebase
+export const deleteDataFromAPI = data => dispatch => {
+  const urlNotes = database.ref(`notes/${data.userId}/${data.noteId}`);
+
+  return new Promise((resolve, reject) => {
+    urlNotes.remove();
+  });
+};
--- src/containers/pages/Dashboard/index.js
+++ src/containers/pages/Dashboard/index.js
@@ -1,13 +1,20 @@
 import React, { Component, Fragment } from "react";
 import "./Dashboard.scss";
 import { connect } from "react-redux";
-import { addDataToAPI, getDataFromAPI } from "../../../config/redux/action";
+import {
+  addDataToAPI,
+  getDataFromAPI,
+  updateDataFromAPI,
+  deleteDataFromAPI
+} from "../../../config/redux/action";
 
 class Dashboard extends Component {
   state = {
     title: "",
     content: "",
-    date: ""
+    date: "",
+    noteId: "",
+    textButton: "SIMPAN"
   };
 
   componentDidMount() {
@@ -16,10 +23,13 @@ class Dashboard extends Component {
   }
 
   handleSaveNotes = () => {
-    const { title, content, date } = this.state;
-    const { saveNotes } = this.props;
+    const { title, content, textButton, noteId } = this.state;
+    const { saveNotes, updateNotes } = this.props;
+
+    // getdata from localstorage
     const userData = JSON.parse(localStorage.getItem("userData"));
 
+    // initialitation data
     const data = {
       title: title,
       content: content,
@@ -27,7 +37,15 @@ class Dashboard extends Component {
       userId: userData.uid
     };
 
-    saveNotes(data);
+    // selection if button equal simpan then running saveNotes function
+    if (textButton === "SIMPAN") {
+      saveNotes(data);
+    } else {
+      // initialitaion noteId and running updateNotes function
+      data.noteId = noteId;
+      updateNotes(data);
+    }
+
     console.log(data);
   };
 
@@ -37,9 +55,40 @@ class Dashboard extends Component {
     });
   };
 
+  updateNotes = note => {
+    console.log(note);
+
+    this.setState({
+      title: note.data.title,
+      content: note.data.content,
+      textButton: "UPDATE",
+      noteId: note.id
+    });
+  };
+
+  cancelUpdate = () => {
+    this.setState({
+      title: "",
+      content: "",
+      textButton: "SIMPAN"
+    });
+  };
+
+  deleteNote = (el, note) => {
+    el.stopPropagation();
+    const { deleteNotes } = this.props;
+    const userData = JSON.parse(localStorage.getItem("userData"));
+    const data = {
+      userId: userData.uid,
+      noteId: note.id
+    };
+    deleteNotes(data);
+  };
+
   render() {
-    const { title, content, date } = this.state;
+    const { title, content, textButton } = this.state;
     const { notes } = this.props;
+    const { updateNotes, cancelUpdate, deleteNote } = this;
     // test
     console.log("notes: ", notes);
 
@@ -58,19 +107,42 @@ class Dashboard extends Component {
             value={content}
             onChange={e => this.onInputChange(e, "content")}
           ></textarea>
-          <button className="save-btn" onClick={this.handleSaveNotes}>
-            Simpan
-          </button>
+          <div className="action-wrapper">
+            {textButton === "UPDATE" ? (
+              <button
+                className="save-btn cancel"
+                onClick={this.handleSaveNotes}
+                onClick={cancelUpdate}
+              >
+                CANCEL
+              </button>
+            ) : (
+              <div></div>
+            )}
+            <button className="save-btn" onClick={this.handleSaveNotes}>
+              {textButton}
+            </button>
+          </div>
         </div>
         <hr />
         {notes.length > 0 ? (
           <Fragment>
             {notes.map(note => {
               return (
-                <div className="card-content" key={note.id}>
+                <div
+                  className="card-content"
+                  key={note.id}
+                  onClick={() => updateNotes(note)}
+                >
                   <p className="title">{note.data.title}</p>
                   <p className="date">{note.data.date}</p>
                   <p className="content">{note.data.content}</p>
+                  <p
+                    className="delete-btn"
+                    onClick={el => deleteNote(el, note)}
+                  >
+                    x
+                  </p>
                 </div>
               );
             })}
@@ -88,7 +160,9 @@ const reduxState = state => ({
 
 const reduxDispatch = dispatch => ({
   saveNotes: data => dispatch(addDataToAPI(data)),
-  getNotes: data => dispatch(getDataFromAPI(data))
+  getNotes: data => dispatch(getDataFromAPI(data)),
+  updateNotes: data => dispatch(updateDataFromAPI(data)),
+  deleteNotes: data => dispatch(deleteDataFromAPI(data))
 });
 
 export default connect(
